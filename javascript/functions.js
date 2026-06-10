5e68e27998a3cbd7960ac12f7840033d610e56a0
Phil Henning <philiphenning@outlook.com>
2026-06-10 16:15:02 +0100

    edited functions page, and updated the homepage to tick off loops

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,7 +4,7 @@
 document.getElementById("Button1").addEventListener("click", changeText);
 document.getElementById("Button2").addEventListener("click", getValue);
 document.getElementById("Button3").onclick = function() {
-    document.getElementById("H2").textContent = "This page is dedicated to working with functions in JavaScript.";
+    document.getElementById("H2").textContent = "This page shows all the different ways to work with functions in JavaScript.";
 }
 
 function changeText() {
