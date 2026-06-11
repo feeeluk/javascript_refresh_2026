65a2c7954621b0bf8c7ceac23f6eac266739061c
Phil Henning <philiphenning@outlook.com>
2026-06-11 09:11:28 +0100

    completed Strings

---
+++ javascript/strings.js
@@ -0,0 +1,11 @@
+// Strings
+
+let stringOne = "double quotes";
+let stringTwo = 'single quotes';
+let stringThree = `string literals`
+let exampleWithEmbededCode = `this is a string literal with a calculation (${3*4*5}) within it`;
+
+console.log(stringOne);
+console.log(stringTwo);
+console.log(stringThree);
+console.log(exampleWithEmbededCode);
