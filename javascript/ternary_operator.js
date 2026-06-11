8402af08a3ccbb2b21c69342dd3eb9fec2caf338
Phil Henning <philiphenning@outlook.com>
2026-06-11 09:01:54 +0100

    completed Ternary Operators

---
+++ javascript/ternary_operator.js
@@ -0,0 +1,16 @@
+// a shortcut to if{} and else{} statements.
+// Helps to assign a variable based on a condition. E.G. condition ? codeIfTrue : codeIfFalse
+
+let age = 18;
+
+let message = age >= 18 ? "you're an adult" : "you're a minor";
+
+console.log("number - " + message);
+
+// Booleans - same idea just a bit simpler
+
+let adult = true;
+
+message = adult ? "you're an adult" : "you're a minor";
+
+console.log(`boolean - ` + message);
