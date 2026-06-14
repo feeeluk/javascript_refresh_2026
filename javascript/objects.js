f76125e2cfe14dc17e4331a8873b1ef3cf0cd743
Phil Henning <philiphenning@outlook.com>
2026-06-14 13:27:16 +0100

    completed Objects, added Classes, Constructors, This, Static, Inheritance, Getters and Setters

---
+++ javascript/objects.js
@@ -0,0 +1,28 @@
+// Example 1
+
+const person = {
+    firstname: "Phil",
+    lastname: "Henning",
+    age: 43,
+    isEmployed: false,
+}
+
+console.log("Example 1:");
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person.isEmployed);
+
+// Example 2
+
+const animal = {
+    name: "Ozzy",
+    type: "dog",
+    age: 4,
+    sound: "woof",
+    makeSound: (sound) => console.log(sound),
+    makeAnotherSound: () => console.log("bark"),
+}
+
+animal.makeSound(animal.sound);
+animal.makeAnotherSound();
