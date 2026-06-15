4a918b3610d42983d1f132a9362752e74ec8dd4f
Phil Henning <philiphenning@outlook.com>
2026-06-15 18:54:07 +0100

    completed first part of Async

---
+++ javascript/async.js
@@ -0,0 +1,31 @@
+// Example 1
+
+    console.log("Example 1:")
+    console.log("Task 1")
+    console.log("Task 2")
+    console.log("Task 3")
+
+    
+// Example 2 - Demonstration of async ('setTimeout()' is natively async)
+    
+    console.log("Example 2:")
+    setTimeout(() => console.log("Task 1"), 1000);
+    console.log("Task 2")
+    console.log("Task 3")
+
+// Example 3 - using Callbacks
+
+    function func1(callback){
+        setTimeout(() => {  console.log("Task 1")
+                            callback()},
+                            1000);
+    }
+
+
+    function func2(){
+        console.log("Task 2");
+        console.log("Task 3");
+    }
+
+    setTimeout(() => console.log("Example 3:"),2000);
+    setTimeout(() => func1(func2), 2001);
