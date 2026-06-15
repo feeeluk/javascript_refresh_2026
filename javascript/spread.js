812430703aa90a09557e413252f4040ba01836fb
Phil Henning <philiphenning@outlook.com>
2026-06-15 21:58:23 +0100

    edited Spread and Rest Parameters

--- javascript/spread.js
+++ javascript/spread.js
@@ -1,25 +1,35 @@
-// ... allows an iterable such as an array or a string to be expanded into seperate elements. Very useful for when passing array variables as parameters to functions.
 
+// Show an array of numbers
+console.log("1:");
 let numbers = [1,2,3,4,5];
-
 console.log(numbers);
 
-let max = Math.max(...numbers);
+// What if we wanted to find the highest number in that array?
+console.log("2:");
+let max = Math.max(numbers);
 console.log(max);
 
+// EXAMPLE
+// What if we wanted to find the highest number in that array?
+console.log("Example 3:");
+let maxWithSpread = Math.max(...numbers);
+console.log(maxWithSpread);
+
+// EXAMPLE
+// an example of method chaining
+console.log("Example 4:");
 let username = "Phil";
-let letters = [...username].join("-"); // an example of method chaining
+let letters = [...username].join("-"); 
 
 console.log(letters);
 
+// EXAMPLE
 // Spread can be used to join two arrays. Well, it lists the individual items, but that can be used to create a new array.
+
+console.log("Example 5:");
 
 let fruits = ["apples", "oranges", "pears"];
 let vegetables = ["potatos", "carrots", "mushrooms"];
-
-console.log(fruits);
-console.log(vegetables);
-
 let foods = [...fruits, ...vegetables];
 
 console.log(foods);
