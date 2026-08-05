43577b4eb230679363e9f8cecfd3da3f96c2a187
Phil Henning <philiphenning@outlook.com>
2026-08-05 13:45:45 +0100

    2nd  save for error handling

--- javascript/advanced/amber/error_handling.js
+++ javascript/advanced/amber/error_handling.js
@@ -3,3 +3,20 @@
 
 document.getElementById("code").onclick = () =>
   window.location = "/javascript/advanced/amber/error_handling.js";
+
+// Example 1
+console.log("Example 1 - No try / catch");
+
+
+// Example 2
+console.log("Example 2 - ");
+
+
+// Example 3
+console.log("Example 3 - ");
+
+// Example 4
+console.log("Example 4 - ");
+
+// Example 5
+console.log("Example 5 - ");
