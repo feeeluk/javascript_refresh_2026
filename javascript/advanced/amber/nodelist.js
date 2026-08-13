489b3e465ebdc8d0a4f0515ffa0d21563f1779f9
Phil Henning <philiphenning@outlook.com>
2026-08-13 17:04:02 +0100

    completed NodeList

--- javascript/advanced/amber/nodelist.js
+++ javascript/advanced/amber/nodelist.js
@@ -1,5 +1,54 @@
-// Nodelist
+// NodeList
 // ////////////////////////////////////////
 
 document.getElementById("code").onclick = () =>
   window.location = "/javascript/advanced/amber/nodelist.js";
+
+
+// Example 1
+
+let buttons = document.querySelectorAll(".nodeListButton"); // create NodeList
+console.log(buttons);
+
+buttons.forEach(button => {
+
+  let clicks = createCounter(); // create a counter for each button
+
+  button.addEventListener("click", event => {
+   
+    clicks.incrementClicks();
+    
+    if(clicks.isEven()){
+      event.target.style.backgroundColor = "hsl(205, 100%, 60%)";
+    }
+    else{
+      event.target.style.backgroundColor = "tomato";
+    }
+
+    console.log(`Button ${button.textContent} - # clicks = ${clicks.getClicks()}`);
+    console.log(`Button ${button.textContent} - ${clicks.isEven()}`);
+  })
+
+})
+
+// Use a Closure to keep count of the number of clicks per button
+
+function createCounter(){
+  
+  let numberOfClicks = 0;
+
+  function incrementClicks(){
+    numberOfClicks++;
+  }
+
+  function getClicks(){
+    return numberOfClicks;
+  }
+
+  function isEven(){
+    return numberOfClicks % 2 === 0; // using the strict comparison makes this return a boolean value
+  }
+
+  return {incrementClicks, getClicks, isEven};
+}
+
