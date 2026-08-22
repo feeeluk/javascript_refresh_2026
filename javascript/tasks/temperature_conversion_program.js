cd152e0d0c8715c93ab2f3fac5ad31c59348f9bc
Phil Henning <philiphenning@outlook.com>
2026-08-22 21:26:48 +0100

    created pages for Temperature Conversion Program

---
+++ javascript/tasks/temperature_conversion_program.js
@@ -0,0 +1,6 @@
+// Temperature Conversion Program
+// ////////////////////////////////////////
+
+document.getElementById("code").addEventListener("click", event => {
+  window.location = "/javascript/tasks/temperature_conversion_program.js";
+});
