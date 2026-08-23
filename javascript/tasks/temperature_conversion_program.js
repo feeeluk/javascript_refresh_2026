3cc2182a5011171e9b554f136bec409b75e61661
Phil Henning <philiphenning@outlook.com>
2026-08-23 12:55:35 +0100

    completed Temperature Conversion Program v3

--- javascript/tasks/temperature_conversion_program.js
+++ javascript/tasks/temperature_conversion_program.js
@@ -1,16 +1,15 @@
 // Temperature Conversion Program
-// ////////////////////////////////////////
+
 
 document.getElementById("code").addEventListener("click", event => {
   window.location = "/javascript/tasks/temperature_conversion_program.js";
 });
 
-// when button is clicked
-document.getElementById("convert").addEventListener("click", event => {
+// ////////////////////////////////////////
 
   // get input temperature
-  const tempInput = document.getElementById("tempInput").value;
-  const temptInputNumber = Number(tempInput);
+  let tempInput;
+  let tempInputNumber;
   
   // get type of conversion
   const toFarenheit = document.getElementById("toFarenheit");
@@ -22,21 +21,33 @@ document.getElementById("convert").addEventListener("click", event => {
   // calculate value
   let calculatedValue;
 
-  if(isNaN(temptInputNumber) || tempInput == ""){
+// when button is clicked
+document.getElementById("convert").addEventListener("click", event => {
+  tempInput = document.getElementById("tempInput").value;
+  tempInputNumber = Number(tempInput);
+
+  if(isNaN(tempInputNumber) || tempInput == ""){
     window.alert("Please enter a NUMBER!");
   }
   else if(toFarenheit.checked){
-    console.log(toFarenheit.value);
-    calculatedValue = (temptInputNumber * 1.8) + 32;
+    calculatedValue = (tempInputNumber * 1.8) + 32;
 
     displayResult.style.display = "block";
     displayResult.textContent = `${calculatedValue.toFixed(1)} °F`;
   }
   else {
-    console.log(toCelsius.value);
-    calculatedValue = (temptInputNumber - 32) / 1.8;
+    calculatedValue = (tempInputNumber - 32) / 1.8;
     
     displayResult.style.display = "block";
     displayResult.textContent = `${calculatedValue.toFixed(1)} °C`;
+  }
+})
+
+// when input is changed to empty
+document.getElementById("tempInput").addEventListener("input", event => {
+  if(event.target.value == ""){
+    displayResult.style.display = "none";
+    tempInput = "";
+    console.log(tempInput);
   }
 })
