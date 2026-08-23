a6a0217db6ea05d18efc341dfbb2e882846d3aac
Phil Henning <philiphenning@outlook.com>
2026-08-23 13:04:39 +0100

    created pages for Dice Roller Program

---
+++ javascript/tasks/dice_roller_program.js
@@ -0,0 +1,8 @@
+// Dice Roller Program
+
+
+document.getElementById("code").addEventListener("click", event => {
+  window.location = "/javascript/tasks/dice_roller_program.js";
+});
+
+// ////////////////////////////////////////
