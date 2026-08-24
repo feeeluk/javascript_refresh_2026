ce9c0347071b417db93376b67c85a5b9a8844847
Phil Henning <philiphenning@outlook.com>
2026-08-24 15:15:30 +0100

    completed Random Password Generator

--- javascript/tasks/random_password_generator.js
+++ javascript/tasks/random_password_generator.js
@@ -7,50 +7,103 @@ document.getElementById("code").addEventListener("click", event => {
 
 // ////////////////////////////////////////
 
+
+
 let passwordLength;
-let includeLower;
-let includeUpper;
-let includeNumber;
-let includeSymbol;
+let includeLowercase;
+let includeUppercase;
+let includeNumbers;
+let includeSymbols;
+
+let allowedChars;
+let password;
+
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!£$%&*@+-";
+
+function reset(){
+
+    allowedChars = "";
+    password = "";
+}
+
+function getOptions(){
+
+    console.log("Options:");
+    
+    passwordLength = document.getElementById("passwordLength").value;
+        console.log(`Pawword length (chars): ${passwordLength}`);
+
+    includeLowercase = document.getElementById("includeLowercase").checked;
+        if(includeLowercase == true){
+            console.log("Lowercase = Yes");
+        }
+
+        else {
+            console.log("Lowercase = No");
+        }
+
+    includeUppercase = document.getElementById("includeUppercase").checked;
+        if(includeUppercase == true){
+            console.log("Uppercase = Yes");
+        }
+        
+        else {
+            console.log("Uppercase = No");
+        }
+
+    includeNumbers = document.getElementById("includeNumbers").checked;
+        if(includeNumbers == true){
+            console.log("Numbers = Yes");
+        }
+        
+        else {
+            console.log("Numbers = No");
+        }
+
+    includeSymbols = document.getElementById("includeSymbols").checked;
+        if(includeSymbols == true){
+            console.log("Symbol = Yes");
+        }
+
+        else {
+            console.log("Symbol = No");
+        }
+
+}
 
 function generatePassword(){
+    
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
 
-    passwordLength = document.getElementById("passwordLength");
-    console.log(passwordLength.value);
+    // console.log(allowedChars);
+    let length = allowedChars.length;
 
-    includeLower = document.getElementById("includeLowercase");
-    if(includeLower.checked){
-        console.log(includeLower.value);
-    }
-    else{
-        console.log("Lowercase = No")
-    }
+    for(let i = 0; i < passwordLength; i++){
+        // console.log(i);
+        let random = Math.floor(Math.random() * length);
+        password += allowedChars[random];
+        
 
-    includeUpper = document.getElementById("includeUppercase");
-        if(includeUpper.checked){
-        console.log(includeUpper.value);
-    }
-    else{
-        console.log("Uppercase = No")
-    }
-
-    includeNumber = document.getElementById("includeNumbers");
-        if(includeNumber.checked){
-        console.log(includeLower.value);
-    }
-    else{
-        console.log("Numbers = No")
-    }
-
-    includeSymbol = document.getElementById("includeSymbols");
-        if(includeSymbol.checked){
-        console.log(includeSymbol.value);
-    }
-    else{
-        console.log("Symbol = No")
     }
 }
 
+function showPassword(){
+    let show = document.getElementById("showPassword");
+    show.style.display = "block";
+    show.innerHTML = `Password = ${password}`;
+    console.log(`Generated Password = ${password}`);
+}
+
 document.getElementById("generatePassword").addEventListener("click", event => {
+    console.clear();
+    reset();
+    getOptions();
     generatePassword();
+    showPassword();
 })
