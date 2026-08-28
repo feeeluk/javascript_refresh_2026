3eeabd2d089f6df18f8fd3f4bef5ec914ff5ecb2
Phil Henning <philiphenning@outlook.com>
2026-08-28 10:28:43 +0100

    completed Hide and Show HTML

--- javascript/tasks/hide_and_show_html.js
+++ javascript/tasks/hide_and_show_html.js
@@ -1,8 +1,25 @@
-// Compound Interest Calculator
+// Hide and Show HTML
 // ////////////////////////////////////////
 
 document.getElementById("code").addEventListener("click", event => {
-  window.location = "/javascript/tasks/compound_interest_calculator.js";
+  window.location = "/javascript/tasks/hide_and_show_html.js";
 });
 
+const button = document.getElementById("myButton");
+const image = document.getElementById("myImage");
 
+document.getElementById("myButton").addEventListener("click", event =>{
+
+  if(button.textContent === "Hide"){
+
+    image.style.visibility = "hidden";
+    button.textContent = "Show"
+
+  }
+  else{
+
+    image.style.visibility = "visible";
+    button.textContent = "Hide"
+
+  }
+})
