d1db7c29d379d3c978c4e5cb04618bf3f033fe72
Phil Henning <philiphenning@outlook.com>
2026-09-01 20:35:46 +0100

    changed the layout entirely (although still very gharish), added logic for the computer's turn, added logic for calculating results.. essentially finished the bare bones task.

--- javascript/tasks/pontoon_1.js
+++ javascript/tasks/pontoon_1.js
@@ -5,24 +5,27 @@
     window.location = "/javascript/tasks/pontoon_1.js";
   });
 
-
 // Variables
 // ////////////////////////////////////////
 
-const actions = document.getElementById("actions");
+const playerActions = document.getElementById("playerActions");
 const startGameButton = document.getElementById("startGame");
 const stickButton = document.getElementById("stick");
 const twistButton = document.getElementById("twist");
 
-const details = document.getElementById("details");
+const playerArea = document.getElementById("playerArea");
 const showPlayerScore = document.getElementById("showPlayerScore");
 const showPlayerNumberOfCards = document.getElementById("showPlayerNumberOfCards");
 const showPlayerCards = document.getElementById("showPlayerCards");
+const showPlayerHandHistory = document.getElementById("showPlayerHandHistory");
+
+const computerArea = document.getElementById("computerArea");
 const showComputerScore = document.getElementById("showComputerScore");
 const showComputerNumberOfCards = document.getElementById("showComputerNumberOfCards");
 const showComputerCards = document.getElementById("showComputerCards");
+const showComputerHandHistory = document.getElementById("showComputerHandHistory");
 
-const results = document.getElementById("results");
+const resultArea = document.getElementById("resultArea");
 const showResults = document.getElementById("showResults");
 
 let deck = [
@@ -32,8 +35,7 @@ let deck = [
               "AS", "KS", "QS", "JS", "10S", "9S", "8S", "7S", "6S", "5S", "4S", "3S", "2S"
             ];
 
-// let deck = [
-//                 "Hearts-Ace", "Hearts-King", "Hearts-Queen"];
+// let deck = ["2H", "3H", "10H", "2S", "3S", "10S", "2D", "3D", "4D", "2C", "3C", "4C"];
 
 let playerScore = 0;
 let playerNumberOfCards = 0;
@@ -46,121 +48,178 @@ let computerCards = [];
 // Functions
 // ////////////////////////////////////////
 
+  function startGame(){
 
-  async function startGame(){
+    startGameButton.disabled = true;
+    
+    setTimeout(() => {dealCards(1, "player")}, 000);
+    setTimeout(() => {dealCards(1, "computer")}, 000);
+    setTimeout(() => {dealCards(1, "player")}, 000);
+    setTimeout(() => {dealCards(1, "computer")}, 000);
 
-    // Hide the 'Start Game' button
-    startGameButton.classList.replace("visible", "hidden");
-
-    // Show the 'details' area
-    details.classList.replace("hidden", "visible");
-
-    // Draw 2 cards for the player
-    await drawCards(2, "player", 1000);
-
-    // Draw 2 cards for the computer
-    await drawCards(2, "computer", 1000);
-
-    // Give player options
-    playerOptions();
+    setTimeout(() => {
+      showCard(showPlayerCards.firstChild, playerCards[0][1]);
+      calculateAndShowValues("player", playerCards[0][1]);
+    }, 000);
+    
+    setTimeout(() => {
+      showCard(showPlayerCards.lastChild, playerCards[1][1]);
+      calculateAndShowValues("player", playerCards[1][1]);
+      playerOptions();
+    }, 000);
+    
   }
 
-  function playerOptions(){
-    
-    if(
-        playerNumberOfCards === 2 &&
-        playerCards.some(card => card.includes("A")) &&
-        (
-          playerCards.some(card => card.includes("K")) ||
-          playerCards.some(card => card.includes("Q")) ||
-          playerCards.some(card => card.includes("J"))
-        )
-    ){
-      // showResults = "You have Pontoon!"
-      results.classList.replace("hidden", "visible");
-      showResults.textContent = "You have PONTOON..";
-    }
-    
-    else if(playerScore < 15){
-      // Show the 'actions' area
-      actions.classList.replace("hidden", "visible");
-
-      // Show the 'twist' button
-      twistButton.classList.replace("hidden", "visible"); 
-    }
-    
-    else if(playerScore >= 15 && playerScore < 21){
-      // Show the 'actions' area
-      actions.classList.replace("hidden", "visible");
-
-      // Show the 'twist' button
-      twistButton.classList.replace("hidden", "visible");
-
-      // Show the 'stick' button
-      stickButton.classList.replace("hidden", "visible"); 
-    }
-
-    else if(playerScore > 21){
-      // Result = "Bust"
-      results.classList.replace("hidden", "visible");
-      showResults.textContent = "Bust";
-
-      // Hide the 'actions' area
-      actions.classList.replace("visible", "hidden");
-    }  
-      
-  }
- 
-  async function drawCards(numberOfCards, who, time){
-
-    function sleep(time){
-      return new Promise(resolve => setTimeout(resolve, time));
-    }
+  function dealCards(numberOfCards, who){
         
-    for(let i = 0; i < numberOfCards; i++){
+    for(let i = 0; i < numberOfCards; i++){     
       
-      const getCard = randomNumber(deck.length); // use the randomNumber function to produce a random number within the size of the deck
+      const getCard = randomNumber(deck.length); // use the randomNumber function to produce a random number within the deck size      
       const chosenCard = deck[getCard]; // use that randon number to get a card out of the deck
-      let value = calculateCardValue(chosenCard); // calculate that card's value (e.g "King" becomes 10)
-        
+      
       deck.splice(getCard, 1); // remove the chosen card from the deck
 
       switch(who){
 
         case "player":
-          playerScore += value; // assign the value of the chosen card to the playerScore variable
-          playerNumberOfCards++; // increase the player's number of cards variable
-          playerCards.push(chosenCard); // assign the chosen card to the playerCards array
-          showPlayerScore.textContent = playerScore;
-          showPlayerNumberOfCards.textContent = playerNumberOfCards;
-          const newPlayerCard = document.createElement("img");
-          newPlayerCard.style.width = "100px";
-          newPlayerCard.src = `/resources/images/cards/front/${chosenCard}.png`;
-          showPlayerCards.append(newPlayerCard);
+          playerCards.push(["back", chosenCard]); // assign the chosen card to the array
+          createCard(showPlayerCards); 
+
           break;
 
-        case "computer":
-          computerScore += value; // assign the value of the chosen card to the computerScore variable
-          computerNumberOfCards++; // increase the computer's number of cards variable
-          computerCards.push(chosenCard); // assign the chosen card to the computerCards array
-          showComputerScore.textContent = computerScore;
-          showComputerNumberOfCards.textContent = computerNumberOfCards;
-          const newComputerCard = document.createElement("img");
-          newComputerCard.style.width = "100px";
-          newComputerCard.src = `/resources/images/cards/front/${chosenCard}.png`;
-          showComputerCards.append(newComputerCard);
+        case "computer":   
+          computerCards.push(["back", chosenCard]); // assign the chosen card to the array
+          createCard(showComputerCards); // create a new img element
+
           break;
-
       }
+    }
+  }
 
-      // show details
+  function handHistory(who, numberOfCards){
 
+    const element = document.createElement("li");
 
-      // only add a delay if there are more cards to come
-      if (numberOfCards - i !== 1) {
-        await sleep(2000);
-      }
+    if(who == "player" && numberOfCards <= 2){
+      element.textContent = "deal";
+      showPlayerHandHistory.append(element);
+    }
+    else if(who == "player" && numberOfCards > 2){
+      element.textContent = "twist";
+      showPlayerHandHistory.append(element);
+    }
+    else if(who == "computer" && numberOfCards <= 2){
+      element.textContent = "deal";
+      showComputerHandHistory.append(element);
+    }
+    else if(who == "computer" && numberOfCards > 2){
+      element.textContent = "twist";
+      showComputerHandHistory.append(element);
+    }
+  }
 
+  function calculateAndShowValues(who, card){
+    
+    let value = calculateCardValue(card); // calculate that card's value (e.g "King" becomes 10)
+
+    switch(who){
+      case "player" :
+        playerScore += value;
+        playerNumberOfCards++;
+        handHistory("player", playerNumberOfCards);
+        showPlayerScore.textContent = playerScore; // show the score
+        showPlayerNumberOfCards.textContent = playerNumberOfCards; // show the number of cards
+        break;
+
+      case "computer" :
+        computerScore += value;
+        computerNumberOfCards++;
+        handHistory("computer", computerNumberOfCards);
+        showComputerScore.textContent = computerScore; // show the score
+        showComputerNumberOfCards.textContent = computerNumberOfCards; // show the number of cards
+        break;
+    }  
+  }
+
+  function createCard(where){
+    let newCard = document.createElement("img");
+    newCard.style.width = "100px"; // give that img element a width
+    newCard.src = `/resources/images/cards/back/back-blue.png`;
+    where.append(newCard); // append the new image to the parent
+  }
+
+  function showCard(htmlParent, card){
+    htmlParent.src = `/resources/images/cards/front/${card}.png`; 
+  }
+
+  function sleep(time){
+      return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+  function playerOptions(){
+
+    twistButton.disabled = true;
+    stickButton.disabled = true;
+    
+    if(
+        playerNumberOfCards === 2 &&
+        playerCards.some(card => card[1].startsWith("A")) &&
+        (
+          playerCards.some(card => card[1].startsWith("K")) ||
+          playerCards.some(card => card[1].startsWith("Q")) ||
+          playerCards.some(card => card[1].startsWith("J"))
+        )
+    ){
+      stickButton.disabled = false; // Enable stick button
+
+      // Add to log
+      let playerHandHistory = document.createElement("li");
+      playerHandHistory.textContent = "You have PONTOON!";
+      showPlayerHandHistory.append(playerHandHistory);
+    }
+
+    else if(playerNumberOfCards === 4 && playerScore >= 15 && playerScore <= 21){
+      twistButton.disabled = false; // Enable 'twist' button
+      stickButton.disabled = false; // Enable the 'stick' button
+
+      // Add to log
+      let playerHandHistory = document.createElement("li");
+      playerHandHistory.textContent = "You have a 4 card hand!";
+      showPlayerHandHistory.append(playerHandHistory);
+    }
+
+    else if(playerNumberOfCards === 5 && playerScore >= 15 && playerScore <= 21){
+      stickButton.disabled = false; // Enable the 'stick' button
+
+      // Add to log
+      let playerHandHistory = document.createElement("li");
+      playerHandHistory.textContent = "You have a 5 card hand!";
+      showPlayerHandHistory.append(playerHandHistory);
+    }
+    
+    else if(playerScore >= 15 && playerScore < 21){
+      twistButton.disabled = false; // Enable 'twist' button
+      stickButton.disabled = false; // Enable the 'stick' button
+    }
+
+    else if(playerScore < 15){
+      twistButton.disabled = false; // Enable 'twist' button
+    }
+
+    else if(playerScore === 21){
+      stickButton.disabled = false; // Enable 'stick' button
+    }
+
+    else if(playerScore > 21){
+      showResults.textContent = "YOU HAVE BUSTED!"; // Result = "Bust"
+    }      
+  }
+
+  function computerOptions(){
+    while(computerScore <=17){
+      dealCards(1, "computer");
+      showCard(showComputerCards.lastChild, computerCards[computerCards.length - 1][1]);
+      calculateAndShowValues("computer", computerCards[computerCards.length - 1][1]);
     }
   }
 
@@ -232,27 +291,87 @@ let computerCards = [];
     return value;
   }
 
-// Page Execution
-// ////////////////////////////////////////
+  function calculateAndShowResults(){
+    if( computerNumberOfCards === 2 &&
+        computerCards.some(card => card[1].startsWith("A")) &&
+        (
+          computerCards.some(card => card[1].startsWith("K")) ||
+          computerCards.some(card => card[1].startsWith("Q")) ||
+          computerCards.some(card => card[1].startsWith("J"))
+        )
+    ){
+      showResults.textContent = "YOU LOSE!";
+    }
 
+    else if(  playerNumberOfCards === 2 &&
+              playerCards.some(card => card[1].startsWith("A")) &&
+              (
+                playerCards.some(card => card[1].startsWith("K")) ||
+                playerCards.some(card => card[1].startsWith("Q")) ||
+                playerCards.some(card => card[1].startsWith("J"))
+              )
+    ){
+      showResults.textContent = "YOU WIN!";
+    }
+
+    else if(  computerScore >= playerScore &&
+              computerScore <= 21
+    ){
+      showResults.textContent = "YOU LOSE!";
+    }
+
+    else if(  computerNumberOfCards == 4 &&
+              computerNumberOfCards >= playerNumberOfCards &&
+              computerScore >= playerScore &&
+              computerScore <= 21
+    ){
+      showResults.textContent = "YOU LOSE!";
+    }
+
+    else if(  computerNumberOfCards == 5 &&
+      computerNumberOfCards >= playerNumberOfCards &&
+      computerScore >= playerScore &&
+      computerScore <= 21
+    ){
+      showResults.textContent = "YOU LOSE!";
+    }
+
+    else {
+      showResults.textContent = "YOU WIN!";
+    }
+  }
 
 // Event Listeners
 // ////////////////////////////////////////
 
   document.getElementById("startGame").addEventListener("click", event => {
-
     startGame();
-    
   })
 
   document.getElementById("stick").addEventListener("click", event => {
-      
-    // Hide player actions
-    // Show computer cards
 
+    // Make the player choices buttons inactive
+    stickButton.disabled = true;
+    twistButton.disabled = true;
+    
+    // Show computer cards
+    setTimeout(() => {
+      showCard(showComputerCards.firstChild, computerCards[0][1]);
+      calculateAndShowValues("computer", computerCards[0][1]);
+    }, 0);
+    
+    setTimeout(() => {
+      showCard(showComputerCards.lastChild, computerCards[1][1]);
+      calculateAndShowValues("computer", computerCards[1][1]);
+      
+      computerOptions();
+      calculateAndShowResults();
+    }, 1000);
   })
 
   document.getElementById("twist").addEventListener("click", event => {
-    drawCards(1, "player", 0);
+    dealCards(1, "player");
+    showCard(showPlayerCards.lastChild, playerCards[playerCards.length - 1][1]);
+    calculateAndShowValues("player", playerCards[playerCards.length - 1][1]);
     playerOptions();
   })
