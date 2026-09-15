4613654c9a3b54bda2e0b87fb6a47a515387957a
Phil Henning <philiphenning@outlook.com>
2026-09-15 13:34:35 +0100

    moved calculateGameResult() to Flow, and created a new function to handle changes to the game state

--- javascript/tasks/pontoon_2_state.js
+++ javascript/tasks/pontoon_2_state.js
@@ -62,62 +62,12 @@
         card.value = value;
     }
     
+    function changeStateOfGame(whichProperty, value)
+    {
+        state[whichProperty] = value;
+    }
+
     function changeStateOfHand(who, whichProperty, value)
     {
         state[who][whichProperty] = value;
-    }
-
-    function calculateGameResult(who)
-    {
-        console.log("calculateGameResult => start");
-        
-        // if Player is bust => Dealer wins
-        if(state.user.handIsBust === true)
-        {
-            state.resultWin = false;
-            state.resultGameOver = true;
-            state.resultMessage = "User is BUST";
-        }
-
-        // if Dealer is bust => Player wins
-
-        // if Player has Pontoon and Dealer does not => Player wins
-
-        // if Dealer has Pontoon and Player does not => Dealer wins
-
-        // if both Player and Dealer have Pontoon => Dealer wins
-
-        // if Player has 5 card hand and Dealer does not => Player wins
-
-        // if Dealer has 5 card hand and Player does not => Dealer wins
-
-        // if both Player and Dealer have 5 card hand, and Player has a higher score => Player wins
-
-        // if both Dealer and Player have 5 card hand, and Dealer has a higher score => Dealer wins
-
-        // if both Dealer and Player have 5 card hand, and they have the same score => Dealer wins
-
-        // if Player has 4 card hand and Dealer does not => Player wins
-
-        // if Dealer has 4 card hand and Player does not => Dealer wins
-
-        // if both Player and Dealer have 4 card hand, and Player has a higher score => Player wins
-
-        // if both Dealer and Player have 4 card hand, and Dealer has a higher score => Dealer wins
-
-        // if both Dealer and Player have 4 card hand, and they have the same score => Dealer wins
-
-        // Player has a higher score => Player wins
-
-        // Dealer has a higher score => Dealer wins
-
-        // both have the same score => Dealer wins
-
-        console.log(`resultGameOver = ${state.resultGameOver}`);
-        console.log(`resultWin = ${state.resultWin}`);
-        console.log(`resultMessage = ${state.resultMessage}`);
-
-        showResultOfGame();
-
-        console.log("calculateGameResult => end");
     }
