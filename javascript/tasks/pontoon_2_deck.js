c0f16e83c1043f65dfc063798518c3f3c08082af
Phil Henning <philiphenning@outlook.com>
2026-09-16 10:26:17 +0100

    created empty function stick()

--- javascript/tasks/pontoon_2_deck.js
+++ javascript/tasks/pontoon_2_deck.js
@@ -16,8 +16,8 @@
     function createDeck()
     {
         const suits = ["H", "D", "S", "C"];
-        const ranks = ["A", "K"];
-        // const ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+        // const ranks = ["A", "K"];
+        const ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
         const values = {
             "A": 0,
             "K": 10,
